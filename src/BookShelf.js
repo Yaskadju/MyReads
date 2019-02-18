89dd0457ddda4405bdc043209c30d4d3fb504d96
yaskadju <thales.y@gmail.com>
2019-02-18 00:44:47 -0300

    Starting to Implement New Components

---
+++ src/BookShelf.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BookShelf from './BookShelf';
+
+class BookShelf extends React.Component {
+    render() {
+        return()
+    }
+}
+
+export default BookShelf;
