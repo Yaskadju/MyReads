563f89b79874adc4d5ca4859878953a55d9acc1b
Yumiya <tyumiya@deloitte.com>
2019-02-20 17:53:14 -0300

    Add shelfchanger component

--- src/Book.js
+++ src/Book.js
@@ -1,41 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ShelfChanger from './ShelfChanger'
 
 class Book extends React.Component {
 
-    static propTypes = {
-        book: PropTypes.object.isRequired
-    }
+static propTypes = {
+    book: PropTypes.object.isRequired
+}
 
-    render() {
+render() {
 
-        const { book } = this.props;
+    const { book } = this.props;
 
-        return (
-            <li key={book.id}>
-                <div className="book">
-                    <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` 
-                }}></div>
-                    <div className="book-shelf-changer">
-                        <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                        </select>
-                    </div>
-                    </div>
-
-                    <div className="book-title">{book.title}</div>
-
-                    {book.authors.map((author) => (
-                        <div className="book-authors">{author}</div>
-                    ))}
+    return (
+        <li key={book.id}>
+            <div className="book">
+                <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` 
+            }}>
+            </div>
+                
+                <ShelfChanger book = {book} />
 
                 </div>
-                </li>
+
+                <div className="book-title">{book.title}</div>
+
+                {book.authors.map((author) => (
+                    <div className="book-authors">{author}</div>
+                ))}
+                
+                </div>
+            </li>
         )
     }
 }
---
+++ src/ShelfChanger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class ShelfChanger extends React.Component {
+static propTypes = {
+    book: PropTypes.object.isRequired
+}
+
+render() {
+
+    const { book } = this.props;
+
+    return (
+        <div className="book-shelf-changer">
+            <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
+        </div>
+        )
+    }
+}
+
+export default ShelfChanger
